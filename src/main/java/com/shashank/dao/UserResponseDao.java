239a7bc8a6992c71e3ab5ae4bce9419f6588dcb6
shashank75 <shashank@zestmoney.in>
2019-07-31 14:18:02 +0530

    first commit

---
+++ src/main/java/com/shashank/dao/UserResponseDao.java
@@ -0,0 +1,15 @@
+package com.shashank.dao;
+
+public class UserResponseDao {
+
+	public String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
